cff8b403d39b30210b68990b586b44afcf12eaa9
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-10-22 19:05:12 +0500

    Task3

---
+++ Tyuiu.ElmurzaevIH.Sprint3.Task3.V22.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ElmurzaevIH.Sprint3.Task3.V22.Lib;
+
+namespace Tyuiu.ElmurzaevIH.Sprint3.Task3.V22.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMaxCharCount()
+        {
+            DataService ds = new DataService();
+            int res = ds.GetMaxCharCount("tbtbbb dsfbg bbg", 'b');
+            int wait = 3;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ElmurzaevIH.Sprint3.Task3.V22.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElmurzaevIH.Sprint3.Task3.V22.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Tyuiu.ElmurzaevIH.Sprint3.Task3.V22.Test")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("130f2a1a-0966-439b-bddd-8b08cb9e8aea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
