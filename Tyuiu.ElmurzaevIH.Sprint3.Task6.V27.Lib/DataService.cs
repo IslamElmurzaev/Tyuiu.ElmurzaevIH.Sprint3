75d1682e03372233bb2d7aa6df39dde9da61834d
Adam Elmurzaev <adamibnkhavaj@gmail.com>
2023-10-29 22:15:46 +0500

    Task6

---
+++ Tyuiu.ElmurzaevIH.Sprint3.Task6.V27.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.ElmurzaevIH.Sprint3.Task6.V27.Lib
+{
+    public class DataService : ISprint3Task6V27
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int sumDiv = 0;
+            int x;
+            
+            for (x = startValue; x <= stopValue; x++)
+            {
+                for (int d = 1;  d <= x; d++)
+                {
+                    if (x % d == 0)
+                    {
+                        if (d > 9)
+                        {
+                            sumDiv += d;
+                        }
+                    }
+                }
+            }
+            return sumDiv;
+        }
+    }
+}
---
+++ Tyuiu.ElmurzaevIH.Sprint3.Task6.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ElmurzaevIH.Sprint3.Task6.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Tyuiu.ElmurzaevIH.Sprint3.Task6.V27.Test")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("873b55d3-6479-44d8-b253-624df4b32be9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
